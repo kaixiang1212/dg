1a2307e88eee884185a5f9549d298304db371bdb
Kai Xiang Yong <z5175681@ad.unsw.edu.au>
2019-08-06 18:50:34 +1000

    reverted original client.cpp

--- client.cpp
+++ client.cpp
@@ -23,51 +23,14 @@ int main() {
 
   std::cout << g << '\n';
 
-  std::vector<std::string> a = g.GetNodes();
-  for(auto i : a) {
-    std::cout << i << "\n";
-  }
-
   gdwg::Graph<std::string, int> g2{g};
 
   std::cout << g2 << "\n";
 
-  std::cout << (g == g2) << "\n";
-
-  g2.DeleteNode("how");
-
-  std::cout << g2 << "\n";
-
-  std::cout << (g == g2) << "\n";
-
   // This is a structured binding.
   // https://en.cppreference.com/w/cpp/language/structured_binding
   // It allows you to unpack your tuple.
-
-  for (gdwg::Graph<std::string,int>::const_iterator it = g.cbegin(); it != g.cend(); it++) {
-    const auto& [from, to, weight] = *it;
-    std::cout << from << " -> " << to << " (weight " << weight << ")\n";
-  }
-
-  /*
-  std::cout << g;
   for (const auto& [from, to, weight] : g) {
     std::cout << from << " -> " << to << " (weight " << weight << ")\n";
   }
-  */
-
-  /*
-  std::cout << g;
-  for (const auto& [from, to, weight] : g) {
-    std::cout << from << " -> " << to << " (weight " << weight << ")\n";
-  }*/
-
-  std::cout << "-----------\n";
-  g.erase("how", "you?", 1);
-
-  for (gdwg::Graph<std::string,int>::const_reverse_iterator it = g.rbegin(); it != g.rend(); it++) {
-    const auto& [from, to, weight] = *it;
-    std::cout << from << " -> " << to << " (weight " << weight << ")\n";
-  }
-
 }
